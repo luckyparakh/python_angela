24129958a19113e5d824b276f351349020c9c355
Rishi Parakh <luckyparakh@gmail.com>
2021-05-15 18:26:37 +0530

    Added TrueFalse Game.

---
+++ TrueFalseGame/data.py
@@ -0,0 +1,49 @@
+question_data = [
+    {"text": "A slug's blood is green.", "answer": "True"},
+    {"text": "The loudest animal is the African Elephant.", "answer": "False"},
+    {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
+    {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
+    {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take "
+             "it home to eat.", "answer": "True"},
+    {"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled "
+             "to a state funeral.", "answer": "False"},
+    {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
+    {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
+    {"text": "Google was originally called 'Backrub'.", "answer": "True"},
+    {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
+    {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
+    {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+]
+
+tdb_question = [
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "The HTML5 standard was published in 2014.", "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "The first computer bug was formed by faulty wires.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "FLAC stands for &quot;Free Lossless Audio Condenser&quot;&#039;",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "The very first recorded computer &quot;bug&quot; was a moth found inside a Harvard Mark II computer.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
+     "question": "The logo for Snapchat is a Bell.", "correct_answer": "False",
+     "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
+     "question": "Pointers were not used in the original C programming language; they were added later on in C++.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "&quot;Windows NT&quot; is a monolithic kernel.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "hard",
+     "question": "The IBM PC used an Intel 8008 microprocessor clocked at 4.77 MHz and 8 kilobytes of memory.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "It&#039;s not possible to format a write-protected DVD-R Hard Disk.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "Android versions are named in alphabetical order.",
+     "correct_answer": "True", "incorrect_answers": ["False"]}
+]
