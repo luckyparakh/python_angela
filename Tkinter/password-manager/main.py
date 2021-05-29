d817d6707459ca60c902ae6a0cf0f6b23a2575af
Rishi Parakh <luckyparakh@gmail.com>
2021-05-29 22:25:53 +0530

    Password Manager

--- Tkinter/password-manager/main.py
+++ Tkinter/password-manager/main.py
@@ -4,6 +4,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import randint, choice, shuffle
 import pyperclip
+import json
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -34,17 +35,52 @@ def save_password():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    data_dict = {
+        website: {
+            'email': email,
+            'password': password
+        }
+    }
     if len(website) == 0:
         messagebox.showerror('Error', 'Website is empty!')
     elif len(password) == 0:
         messagebox.showerror('Error', 'Password is empty!')
     else:
-        detail = f"{website} | {email} | {password}"
-        with open('details.text', 'a') as file:
-            file.write(detail + "\n")
-        messagebox.showinfo("Saved", "Password Saved!!")
-        website_entry.delete(0, END)
-        password_entry.delete(0, END)
+        try:
+            with open('data.json', 'r') as file:
+                # Reading Old Data
+                json_data = json.load(file)
+        except FileNotFoundError:
+            with open('data.json', 'w') as file:
+                # Writing data into file
+                json.dump(data_dict, file, indent=4)
+                messagebox.showinfo("Saved", "Password Saved!!")
+        else:
+            # Updating old data with new data
+            json_data.update(data_dict)
+            with open('data.json', 'w') as file:
+                # Writing updated data into file
+                json.dump(json_data, file, indent=4)
+                messagebox.showinfo("Saved", "Password Saved!!")
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+
+
+# ---------------------------- Search------------------------------- #
+def search():
+    website = website_entry.get()
+    try:
+        with open('data.json', 'r') as data_file:
+            data_dict = json.load(data_file)
+            try:
+                email = data_dict[website]['email']
+                password = data_dict[website]['password']
+                messagebox.showinfo(website, f"Email: {email}\nPassword: {password}")
+            except KeyError as error_message:
+                messagebox.showerror("Error", f"Key: {error_message} not found.")
+    except FileNotFoundError:
+        messagebox.showerror("Error", "No data file found.")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -59,9 +95,12 @@ canvas.grid(row=0, column=1, columnspan=3)
 website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
 
-website_entry = Entry(width=41)
+website_entry = Entry(width=21)
 website_entry.focus()
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.grid(row=1, column=1)
+
+search_button = Button(text="Search", command=search, width=14)
+search_button.grid(row=1, column=2)
 
 email_label = Label(text="Email/Username:")
 email_label.grid(row=2, column=0)
